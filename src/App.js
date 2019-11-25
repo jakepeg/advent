13a73f0ea09d53eec9d4cd11681fa281e9aabc55
Developer <Dev@jacobs-macbook-pro.home>
2019-11-25 14:37:32 +0100

    cookie consent, layout tweaks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import data from "./doors.json";
+import { Helmet } from "react-helmet";
+import CookieConsent from "react-cookie-consent";
 
 function App() {
   const clearLocalData = () => {
@@ -15,7 +17,6 @@ function App() {
   const today = new Date();
 
   const openDoor = e => {
-    let correctedDay = parseInt(e.currentTarget.id) - parseInt(1);
     if (today.getDate() >= e.currentTarget.id && today.getMonth() === month) {
       e.currentTarget.childNodes[0].classList.add("opened");
       e.currentTarget.parentElement.classList.add("glow");
@@ -27,33 +28,43 @@ function App() {
   };
 
   return (
-    <ul className="image-grid">
-      {data.map(item => (
-        <li
-          key={item.door}
-          className={
-            JSON.stringify(localData).includes(item.door) ? "glow" : ""
-          }
-        >
-          <div onClick={openDoor} id={item.door} className="flip-container">
-            <div
-              className={
-                JSON.stringify(localData).includes(item.door)
-                  ? "flipper opened"
-                  : "flipper"
-              }
-            >
-              <div className="front">
-                <p>{item.door}</p>
-              </div>
-              <div className="back">
-                <img key={item.door} src={item.image} alt="xmas" />
+    <div>
+      <Helmet>
+        <title>Advent Calendar</title>
+        <meta name="description" content="Online Advent Calendar" />
+      </Helmet>
+      <h1>Christmas is coming!</h1>
+      <ul className="image-grid">
+        {data.map(item => (
+          <li
+            key={item.door}
+            className={
+              JSON.stringify(localData).includes(item.door) ? "glow" : ""
+            }
+          >
+            <div onClick={openDoor} id={item.door} className="flip-container">
+              <div
+                className={
+                  JSON.stringify(localData).includes(item.door)
+                    ? "flipper opened"
+                    : "flipper"
+                }
+              >
+                <div className="front">
+                  <p>{item.door}</p>
+                </div>
+                <div className="back">
+                  <img key={item.door} src={item.image} alt="xmas" />
+                </div>
               </div>
             </div>
-          </div>
-        </li>
-      ))}
-    </ul>
+          </li>
+        ))}
+      </ul>
+      <CookieConsent>
+        This website uses cookies to enhance the user experience.
+      </CookieConsent>
+    </div>
   );
 }
 
