d49072c8f72921464f9420becd4846fe55b183c6
Developer <Dev@jacobs-macbook-pro.home>
2019-11-27 10:24:14 +0100

    fix form

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -57,33 +57,16 @@ function Footer() {
           Get reminders
           <br />
           <form name="contact" method="POST" data-netlify="true">
-            <p>
-              <label>
-                Your Name: <input type="text" name="name" />
-              </label>
-            </p>
-            <p>
-              <label>
-                Your Email: <input type="email" name="email" />
-              </label>
-            </p>
-            <p>
-              <label>
-                Your Role:{" "}
-                <select name="role[]" multiple>
-                  <option value="leader">Leader</option>
-                  <option value="follower">Follower</option>
-                </select>
-              </label>
-            </p>
-            <p>
-              <label>
-                Message: <textarea name="message"></textarea>
-              </label>
-            </p>
-            <p>
-              <button type="submit">Send</button>
-            </p>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="email address"
+              required
+            />
+            <button className="formSend" type="submit">
+              Sign up
+            </button>
           </form>
         </div>
       </div>
